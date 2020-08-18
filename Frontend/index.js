628330b927f715cfef23ebe19c3d77f89b3292c4
Franck Michaud <ludwig-f.369@9online.fr>
2020-08-18 18:43:07 +0200

    first commit

---
+++ Frontend/index.js
@@ -0,0 +1,13 @@
+
+//requette API
+let request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        let response = JSON.parse(this.responseText);
+        console.log(response);
+    }
+};
+request.open("GET", "http://localhost:3000/api/cameras");
+request.send();
+
+showTotalInPanier();
