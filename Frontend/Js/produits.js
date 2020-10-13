fb399f04e7e4cb4e7413186c6ab4f5c179ac7169
Franck Michaud <ludwig-f.369@9online.fr>
2020-10-13 20:12:43 +0200

    finsihed

--- Frontend/Js/produits.js
+++ Frontend/Js/produits.js
@@ -3,14 +3,14 @@ function purchase(newProduct) {
     console.log('purchase');
     window.location = 'panier.html'
 
-    //Envoi produit dans panier
-    let camera = new Product(item.lenses, item.name, item.description, item.imageUrl, item.price);
+    //Copie de l'objet item dans la variable camera
+    let camera = item;
 
-    //selctionner la lentille
+    //recuperation de la valeur de la lentille
     const selectedValue = document.getElementById("select").value;
         console.log(selectedValue)
 
-    //mettre la lentille dans le panier
+    //création d'un nouvel objet et enregistrement dans le panier
     let article = {"name": camera.name, "price": camera.price/100, "id": camera._id, "imageUrl": camera.imageUrl,"lenses": selectedValue, "qty": 1};
     let panier = JSON.parse(localStorage.getItem('keyPanier')) || [];
         panier.push(article);
@@ -30,72 +30,57 @@ const middle = document.getElementById('middle');
 let item;
 
 fetch("http://localhost:3000/api/cameras/" + params.get('id'))
-    .then(response => {
-        console.log(response);
-        if (response.ok) {
-            return response.json();
-        } else {
-            return Promise.reject(response.status);
-        }
+.then(response => {
+    console.log(response);
+    if (response.ok) {
+        return response.json();
+    } else {
+        return Promise.reject(response.status);
+    }
+})
+.then(camera => {
+    console.log(camera.lenses);
+
+    //affichage de la carte de chaque photo
+    let card =
+        `<div class="col mb-4">
+            <div class="card">
+                <h5 class="card-title">${camera.name}</h5>
+                    <img src="${camera.imageUrl}" class="card-img-top" alt="${camera.name}">
+                        <div class="card-body">
+                            <select id="select">
+                                <option selected disabled >Optiques</option>
+                            </select>
+                        </div>
+                        
+                        <br />
+                        <div class="details">
+                            <p class="card-text">${camera.description}</p>
+                            <p class="card-text">${camera.price / 100}€</p>
+                                <button id="buy" type="button" class="btn btn-primary">Acheter</button>
+                        </div>
+            </div>
+         </div>`;
+
+    middle.innerHTML = card;
+
+    const select = document.getElementById("select");//pointe vers #select dans notre HTML
+    for (let i in camera.lenses){ //boucle for pour les lenses
+    select.innerHTML += `<option value='${camera.lenses[i]}' selected='selected' > ${camera.lenses[i]}</option>`
+    }
+
+    //cliquer sur bouton pour acheter l'objet
+    const buy = document.getElementById('buy');
+
+    buy.addEventListener('click', function() {
+        purchase(camera);
     })
-    .then(item => {
-        let camera = new Product( item.lenses, item.name, item.description, item.imageUrl, item.price);
-        console.log(camera.lenses);
 
-        //affichage de la carte de chaque photo
-        let card =
-            `<div class="col mb-4">
-                <div class="card">
-                    <h5 class="card-title">${camera.name}</h5>
-                        <img src="${camera.imageUrl}" class="card-img-top" alt="${camera.name}">
-                            <div class="card-body">
-                                <select id="select">
-                                    <option selected disabled >Optiques</option>
-                                </select>
-                            </div>
-                            
-                            <br />
-                            <div class="details">
-                                <p class="card-text">${camera.description}</p>
-                                <p class="card-text">${camera.price / 100}€</p>
-                                    <button id="buy" type="button" class="btn btn-primary">Acheter</button>
-                            </div>
-                </div>
-             </div>`;
-
-        middle.innerHTML = card;
-
-        const select = document.getElementById("select");//pointe vers #select dans notre HTML
-        for (let i in camera.lenses){ //boucle for pour les lenses
-        select.innerHTML += `<option value='${camera.lenses[i]}' selected='selected' > ${camera.lenses[i]}</option>`
-        }
-
-        //cliquer sur bouton pour acheter l'objet
-        const buy = document.getElementById('buy');
-
-        buy.addEventListener('click', function() {
-            purchase(camera);
-        })
-
-        item = camera;
-        console.log(item);
-        });
+    item = camera;
+    console.log(item);
+});
 
 
-class Product {
-    constructor(lenses, name, description, imageUrl, price) {
-        this.lenses = lenses;
-        this.name = name;
-        this.description = description;
-        this.lenses = lenses;
-        this.imageUrl = imageUrl;
-        this.price = price;
-        this.status = 'null';
-    }
-    buyProduct() {
-        this.status = 'buy';
-    }
-}
 
 
 
